0a61c5af75f11dab01d57a2e3b3051e77b9b29f1
M4xM3kk3r <jonkvanum@gmail.com>
2024-08-20 00:47:27 +0200

    Update ABCformel.py

--- StartEksempel/ABCformel.py
+++ StartEksempel/ABCformel.py
@@ -6,19 +6,33 @@ Koeffisientane i polynomet er hard-koda inn i starten.
 # Importere NumPy
 import numpy as np
 
-# Gir koeffisientane
-a = 1
-b = -3
-c = 2
+#funksjon
+def func(a,b,c):
+    #diskriminant
 
-# Reknar ut løysingane - dersom dei er reelle
-if b**2-4*a*c >= 0:
-    x_1 = (-b - np.sqrt(b**2-4*a*c)) / (2*a)
-    x_2 = (-b + np.sqrt(b**2-4*a*c)) / (2*a)
+    diskriminant = b**2-4*a*c
+    #diskriminant negativ?
+    if diskriminant >= 0:
+        x_1 = (-b - np.sqrt(b**2-4*a*c)) / (2*a)
+        x_2 = (-b + np.sqrt(b**2-4*a*c)) / (2*a)
 
     # Skriv løysingane til skjerm
-    print('x1 =', x_1)
-    print('x2 =', x_2)
-else:
-    print('Likninga har ingen reelle løysingar')
-      
+        print('x1 =', x_1)
+        print('x2 =', x_2)
+    else:
+        print('Likninga har ingen reelle løysingar')
+        
+# Gir koeffisientane
+while True:
+    a = int(input("a="))
+    b = int(input("b="))             
+    c = int(input("c="))
+
+    print(a,b,c)
+
+    func(a,b,c)
+
+
+
+    # Reknar ut løysingane - dersom dei er reelle
+
